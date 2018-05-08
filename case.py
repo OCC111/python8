e877b8d40b8e24b1716ba39b89da06f65f403ed8
OCC111 <15600686330@163.com>
2018-05-08 15:26:39 +0800

    This content is open source and prohibited from selling

---
+++ case.py
@@ -0,0 +1,17 @@
+a=90
+b=70
+c=50
+c=a+b
+print ("c-1的值:",c)
+c=a-b
+print ("c-2的值:",c)
+c=a*b
+print ("c-3的值:",c)
+c=a/b 
+print ("c-4的值:",c)
+a=400
+b=600
+c=300
+c=a**b
+print ("c-5的值:",c)
+# python8
